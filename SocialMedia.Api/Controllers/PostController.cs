d0ab976da065d91311ec613bfc26b8b585e5438c
Juan Sosa <sosajseba@gmail.com>
2020-10-18 21:01:38 -0300

    add post data transfer object

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
@@ -20,19 +22,43 @@ namespace SocialMedia.Api.Controllers
         public async Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
-            return Ok(posts);
+            var postsDto = posts.Select(x => new PostDto
+            {
+                PostId = x.PostId,
+                Description = x.Description,
+                Image = x.Image,
+                UserId = x.UserId,
+                Date = x.Date
+            });
+
+            return Ok(postsDto);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
             var post = await _postRepository.GetPost(id);
-            return Ok(post);
+            var postDto = new PostDto
+            {
+                PostId = post.PostId,
+                Description = post.Description,
+                Image = post.Image,
+                UserId = post.UserId,
+                Date = post.Date
+            };
+            return Ok(postDto);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(Post post)
+        public async Task<IActionResult> Post(PostDto postDto)
         {
+            var post = new Post
+            {
+                Description = postDto.Description,
+                Image = postDto.Image,
+                UserId = postDto.UserId,
+                Date = postDto.Date
+            };
             await _postRepository.InsertPost(post);
             return Ok(post);
         }
