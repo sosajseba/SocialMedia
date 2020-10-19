bfe3e06c2843a2312a5dce5cb5ba4f967713575e
Juan Sosa <sosajseba@gmail.com>
2020-10-19 12:45:15 -0300

    add custom exceptions

---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository : BaseRepository<Post>, IPostRepository
+    {
+        public PostRepository(SocialMediaContext context) : base(context) { }
+
+        public async Task<IEnumerable<Post>> GetPostsByUser(int id)
+        {
+            return await _entities.Where(x => x.UserId == id).ToListAsync();
+        }
+    }
+}
